b8f982fddc696c89b31dccca41c34de405b7cc8f
Deivy38 <jhonny38camacho@gmail.com>
2026-05-19 21:31:38 -0500

    aplicacion de saludos

--- personas.py
+++ personas.py
@@ -1,4 +1,12 @@
-personas = []
+personas = [{
+    "nombre": "carlos",
+    "edad": 30,
+    "ciudad": "Colombia"
+}, {
+    "nombre": "camilo",
+    "edad": 30,
+    "ciudad": "Colombia"
+}]
 
 usuario = { "nombre": "Juan", "edad": "30", "ciudad": "Colombia" }
 
@@ -22,3 +30,39 @@ usuario["ciudad"].append("colombia")
 usuario["ciudad"].append("bogota")
 
 print(usuario)
+
+usuario1 = {
+    "nombre": "Juan",
+    "edad": 30,
+    "ciudad": "Colombia"
+}
+
+usuario2 = {
+    "nombre": "Maria",
+    "edad": 25,
+    "ciudad": "México"
+}
+
+usuario3 = {
+    "nombre": "Carlos",
+    "edad": 28,
+    "ciudad": "Perú"
+}
+
+usuario4 = {
+    "nombre": "Ana",
+    "edad": 22,
+    "ciudad": "Argentina"
+}
+
+usuario5 = {
+    "nombre": "Luis",
+    "edad": 35,
+    "ciudad": "Chile"
+}
+
+personas.append(usuario1)
+personas.append(usuario2)   
+personas.append(usuario3)
+personas.append(usuario4)
+personas.append(usuario5)
